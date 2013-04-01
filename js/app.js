9815c5fb7975d7c01cc0f3cb09fff19b90906ce8
William Grasel <d.rufo.s@gmail.com>
2013-04-01 17:50:55 -0300

    ajuste inicial das rotas

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+    this.route("home", { path: "/" });
+    this.route("endereco");
+    this.route("about");
 });
 
 App.IndexRoute = Ember.Route.extend({
