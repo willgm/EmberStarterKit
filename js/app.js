f3583b911a98439cc2ddc3852f14bc76b419e962
William Grasel <d.rufo.s@gmail.com>
2013-04-02 01:03:07 -0300

    inserindo tabela de listagem de endereços

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 App = Ember.Application.create();
 
+App.Base = [
+        {
+            estado: 'SP',
+            logradouro: 'teste'
+        }
+    ];
+
 App.Router.map(function() {
     this.route("home", { path: "/" });
     this.route("endereco");
     this.route("about");
 });
 
-App.IndexRoute = Ember.Route.extend({
+App.EnderecoRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return App.Base;
   }
 });
